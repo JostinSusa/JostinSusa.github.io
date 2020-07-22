adeffcb1f039665f38dda9ca8f1053f2d4fb3101
JostinSusa <jostinadrian@gmail.com>
2020-07-22 16:50:51 -0500

    Priper commit con el nuevo diseño del sitio, desarrollado sin diseño responsivo hasta la seccion de conocimientos

---
+++ js/main.js
@@ -0,0 +1,67 @@
+var front = document.getElementById('front'),
+	back = document.getElementById('back'),
+	version = document.getElementById('version'),
+	diseno = document.getElementById('diseno');
+
+var hFront = document.getElementById('hFront'),
+	hBack = document.getElementById('hBack'),
+	hVersion = document.getElementById('hVersion'),
+	hDiseno = document.getElementById('hDiseno');
+
+	(function (){
+		if (hFront.classList == 'active'){
+			front.style.display = 'flex';
+			back.style.display = '';
+			version.style.display = '';
+			diseno.style.display = '';
+		}
+	}());
+
+	hFront.addEventListener('click', function(){
+		hFront.className = 'active';
+		hBack.className = '';
+		hVersion.className = '';
+		hDiseno.className = '';
+		if (hFront.classList == 'active'){
+			front.style.display = 'flex';
+			back.style.display = '';
+			version.style.display = '';
+			diseno.style.display = '';
+		}
+	});
+	hBack.addEventListener('click', function(){
+		hFront.className = '';
+		hBack.className = 'active';
+		hVersion.className = '';
+		hDiseno.className = '';
+		if (hBack.classList == 'active'){
+			front.style.display = '';
+			back.style.display = 'flex';
+			version.style.display = '';
+			diseno.style.display = '';
+		}
+	});
+	hVersion.addEventListener('click', function(){
+		hFront.className = '';
+		hBack.className = '';
+		hVersion.className = 'active';
+		hDiseno.className = '';
+		if (hVersion.classList == 'active'){
+			front.style.display = '';
+			back.style.display = '';
+			version.style.display = 'flex';
+			diseno.style.display = '';
+		}
+	});
+	hDiseno.addEventListener('click', function(){
+		hFront.className = '';
+		hBack.className = '';
+		hVersion.className = '';
+		hDiseno.className = 'active';
+		if (hDiseno.classList == 'active'){
+			front.style.display = '';
+			back.style.display = '';
+			version.style.display = '';
+			diseno.style.display = 'flex';
+		}
+	});
